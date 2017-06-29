6e39cd8e64ad80df3363861f991a0662e2abcfeb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-06-29 21:35:43 +0100

    Use the dev-screen-record as a base point

---
+++ src/test/java/acceptance/CanPlayVideoTest.java
@@ -0,0 +1,26 @@
+package acceptance;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import tdl.record.image.output.OutputToScreen;
+import tdl.record.video.VideoPlayer;
+
+import java.util.concurrent.TimeUnit;
+
+public class CanPlayVideoTest {
+
+    @Test
+    @Ignore("Manual acceptance")
+    public void play_recorded_video() throws Exception {
+        VideoPlayer videoPlayer = new VideoPlayer(new OutputToScreen());
+
+        videoPlayer.open("./build/text.mp4");
+        videoPlayer.seekTo(2, TimeUnit.SECONDS);
+        videoPlayer.seekTo(5, TimeUnit.SECONDS);
+        videoPlayer.play();
+
+        videoPlayer.close();
+    }
+
+
+}
