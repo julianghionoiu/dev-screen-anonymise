3118c184869dee28ea70537eb2752dbb2c9311df
petra <Petra Barus>
2017-07-03 20:38:57 +0700

    Add more testcases for multiple subimages.

--- src/test/java/acceptance/CanRemoveSubimageTest.java
+++ src/test/java/acceptance/CanRemoveSubimageTest.java
@@ -41,24 +41,32 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import tdl.anonymize.video.VideoMasker;
+import tdl.record.video.VideoRecorderException;
 
 public class CanRemoveSubimageTest {
 
+    private static String destinationVideo = "build/recording_from_barcode_at_4x.mp4";
+
+    private static TimeSource recordTimeSource = new FakeTimeSource();
+    
+    private static ImageInput imageInput = new InputFromStreamOfBarcodes(BarcodeFormat.CODE_39, 300, 150, recordTimeSource);
+
+    @Before
+    public void generate_video() throws VideoRecorderException {
+        //Creating video
+        if (!Files.exists(Paths.get(destinationVideo))) {
+            VideoRecorder videoRecorder = new VideoRecorder.Builder(imageInput).withTimeSource(recordTimeSource).build();
+            // Capture video
+            videoRecorder.open(destinationVideo, 5, 4);
+            videoRecorder.start(Duration.of(12, ChronoUnit.SECONDS));
+            videoRecorder.close();
+        }
+    }
+
     @Test
     public void can_remove_subimage() throws Exception {
-
-        //Creating video
-        String destinationVideo = "build/recording_from_barcode_at_4x.mp4";
-        TimeSource recordTimeSource = new FakeTimeSource();
-        ImageInput imageInput = new InputFromStreamOfBarcodes(BarcodeFormat.CODE_39, 300, 150, recordTimeSource);
-        VideoRecorder videoRecorder = new VideoRecorder.Builder(imageInput).withTimeSource(recordTimeSource).build();
-
-        // Capture video
-        videoRecorder.open(destinationVideo, 5, 4);
-        videoRecorder.start(Duration.of(12, ChronoUnit.SECONDS));
-        videoRecorder.close();
-
         String destinationImage = "build/barcode_subimage.png";
         recordTimeSource.wakeUpAt(1, TimeUnit.SECONDS);
         BufferedImage bufferedImage = imageInput.readImage();
@@ -73,5 +81,39 @@ public class CanRemoveSubimageTest {
                 Arrays.asList(new Path[]{Paths.get(destinationImage)})
         );
         masker.run();
+        int[] affectedFrames = new int[] {};
+        //TODO: Read video frame and see if the affected frames matches
+    }
+    
+    @Test
+    public void can_remove_multiple_in_one_frame() throws Exception {
+        String maskedDestination = "build/recording_from_barcode_at_4x.masked.1.mp4";
+
+        Path subImage1 = Paths.get("src/test/resources/barcode-subimage-1.png");
+        Path subImage2 = Paths.get("src/test/resources/barcode-subimage-2.png");
+        VideoMasker masker = new VideoMasker(
+                Paths.get(destinationVideo),
+                Paths.get(maskedDestination),
+                Arrays.asList(new Path[]{subImage1, subImage2})
+        );
+        masker.run();
+        int[] affectedFrames = new int[] {};
+        //TODO: Read video frame and see if the affected frames matches
+    }
+    
+    @Test
+    public void can_remove_multiple_subimages_in_different_frame() throws Exception {
+        String maskedDestination = "build/recording_from_barcode_at_4x.masked.2.mp4";
+
+        Path subImage1 = Paths.get("src/test/resources/barcode-subimage-1.png");
+        Path subImage2 = Paths.get("src/test/resources/barcode-subimage-3.png");
+        VideoMasker masker = new VideoMasker(
+                Paths.get(destinationVideo),
+                Paths.get(maskedDestination),
+                Arrays.asList(new Path[]{subImage1, subImage2})
+        );
+        masker.run();
+        int[] affectedFrames = new int[] {};
+        //TODO: Read video frame and see if the affected frames matches
     }
 }
